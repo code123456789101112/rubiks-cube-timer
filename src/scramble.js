819403d0fd70d2164927b1be1361656e81791cd5
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-09-11 11:43:15 -0400

    feat: Add 5x5x5, 6x6x6, and 7x7x7 scramble options

--- src/scramble.js
+++ src/scramble.js
@@ -2,14 +2,44 @@ function generateScramble(cube = "3x3x3") {
     const nums = [0, 1, 2];
     const baseMoves = ["R", "U", "F"];
 
-    const size = cube === "3x3x3" ? 20 : cube === "4x4x4" ? 45 : 11;
+    const size =
+        cube === "3x3x3"
+            ? 20
+            : cube === "4x4x4"
+            ? 45
+            : cube === "5x5x5"
+            ? 60
+            : cube === "6x6x6"
+            ? 80
+            : cube === "7x7x7"
+            ? 100
+            : 11;
 
-    if (cube === "3x3x3") {
+    const cubeSize = parseInt(cube.slice(0, 1));
+
+    if (cubeSize >= 3) {
         nums.push(3, 4, 5);
         baseMoves.push("L", "B", "D");
-    } else if (cube === "4x4x4") {
-        nums.push(3, 4, 5, 6, 7, 8);
-        baseMoves.push("L", "B", "D", "Rw", "Uw", "Fw");
+    }
+
+    if (cubeSize >= 4) {
+        nums.push(6, 7, 8);
+        baseMoves.push("Rw", "Uw", "Fw");
+    }
+
+    if (cubeSize > 5) {
+        nums.push(9, 10, 11);
+        baseMoves.push("Lw", "Bw", "Dw");
+    }
+
+    if (cubeSize > 6) {
+        nums.push(12, 13, 14);
+        baseMoves.push("3Rw", "3Uw", "3Fw");
+    }
+
+    if (cubeSize > 7) {
+        nums.push(15, 16, 17);
+        baseMoves.push("3Lw", "3Bw", "3Dw");
     }
 
     let bad = true;
